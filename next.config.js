fcda5ad542932ca0f628180c6435c8494283a014
transport <govindguptasitamadhi@gmail.com>
2026-08-18 09:55:01 +0530

    Fix basePath configuration for GitHub Pages
    Updated basePath for GitHub Pages deployment.

--- next.config.js
+++ next.config.js
@@ -1,17 +1,8 @@
 /** @type {import('next').NextConfig} */
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
-
-let repo = '';
-if (isGithubActions) {
-  // यह repository नाम निकालेगा (e.g., 'bjpmuzwest')
-  repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
-}
-
 const nextConfig = {
   output: 'export',
-  // GitHub Pages के Repository path के लिए
-  basePath: isGithubActions ? `/${repo}` : '',
-  assetPrefix: isGithubActions ? `/${repo}/` : '',
+  // आपकी Repository का सटीक नाम
+  basePath: '/bjpmuzwest',
   images: {
     unoptimized: true,
   },
