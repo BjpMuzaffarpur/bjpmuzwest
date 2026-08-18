22f5c2db65a7c7e9ac76ffb4afc2aa4988c0d0f1
transport <govindguptasitamadhi@gmail.com>
2026-08-18 09:49:17 +0530

    Update next.config.js for GitHub Actions support

--- next.config.js
+++ next.config.js
@@ -1,11 +1,20 @@
 /** @type {import('next').NextConfig} */
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let repo = '';
+if (isGithubActions) {
+  // यह repository नाम निकालेगा (e.g., 'bjpmuzwest')
+  repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
+}
+
 const nextConfig = {
   output: 'export',
-  basePath: '/bjpmuzwest',
-  trailingSlash: true,
+  // GitHub Pages के Repository path के लिए
+  basePath: isGithubActions ? `/${repo}` : '',
+  assetPrefix: isGithubActions ? `/${repo}/` : '',
   images: {
     unoptimized: true,
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
