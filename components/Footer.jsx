70965aa4406c7f746f1b3cc640bf52282cbe07e7
transport <govindguptasitamadhi@gmail.com>
2026-08-18 09:14:22 +0530

    Change contact email and update footer information
    Updated contact email and footer text for clarity.

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -22,13 +22,13 @@ export default function Footer() {
           <div className="footer-col">
             <h4 data-lang="en">Contact</h4><h4 data-lang="hi">संपर्क</h4>
             <ul>
-              <li><a href="mailto:info@bjpmuzaffarpurwest.example">info@bjpmuzaffarpurwest.example</a></li>
+              <li><a href="mailto:bjp95kanti@gmail.com">bjp95kanti@gmail.com</a></li>
             </ul>
           </div>
         </div>
         <div className="footer-bottom">
-          <span data-lang="en">&copy; 2026 Bharatiya Janata Party. All rights reserved.</span>
-          <span data-lang="hi">&copy; 2026 भारतीय जनता पार्टी। सर्वाधिकार सुरक्षित।</span>
+          <span data-lang="en">&copy; 2026 Bharatiya Janata Party Muzaffarpur West. All rights reserved.</span>
+          <span data-lang="hi">&copy; 2026 भारतीय जनता पार्टी मुजफ्फरपुर पश्चिमी। सर्वाधिकार सुरक्षित।</span>
         </div>
       </div>
 
