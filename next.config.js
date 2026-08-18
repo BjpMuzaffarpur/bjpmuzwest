7c8edc1fac9de6e7c02969cf8e45950bc35bf0cd
transport <govindguptasitamadhi@gmail.com>
2026-08-18 08:16:15 +0530

    Delete next.config.js

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export', // यह आपके प्रोजेक्ट को HTML में बदल देगा
-  images: {
-    unoptimized: true, // GitHub pages पर इमेजेज के लिए यह जरूरी है
-  }
-}
-module.exports = nextConfig
