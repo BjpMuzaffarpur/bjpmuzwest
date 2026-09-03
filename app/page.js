1ceda1703e54fb0ac2e1ed42f26a1641c5eb7c0f
transport <govindguptasitamadhi@gmail.com>
2026-09-03 17:27:50 +0530

    Add Upcoming and Events components to page

--- app/page.js
+++ app/page.js
@@ -3,6 +3,8 @@ import Hero from '@/components/Hero';
 import About from '@/components/About';
 import Organization from '@/components/Organization';
 import Setup from '@/components/Setup';
+import Upcoming from '@/components/UpcomingEvents';
+import Events from '@/components/Events';
 import Gallery from '@/components/Gallery';
 import OfficeBearers from '@/components/OfficeBearers';
 import News from '@/components/News';
@@ -16,6 +18,8 @@ export default function HomePage() {
       <About />
       <Organization />
       <Setup />
+      <Upcoming />
+      <Events />
       <OfficeBearers />
       <Gallery />
       <News />
